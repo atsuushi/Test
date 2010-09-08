032dac8a245bea1ecdb25dc4cb3356264d316ab0
atsuushi <atsuushi@GMail.com>
2010-09-09 01:12:02 +0900

    -

---
+++ src/localhost/Hello.java
@@ -0,0 +1,15 @@
+package localhost;
+
+/**
+ * Hello.
+ */
+public final class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        System.out.println("TEST");
+	}
+
+}
